a3ab2e7cff336fa32fd1ff6eb065b00367891e88
Bj-LAI <bjornlai13@gmail.com>
2025-05-29 12:36:04 +0200

    Update config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://bj-lai.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/little-sandman-docs3/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
